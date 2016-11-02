dc52d580364c2b0aa633eb752610ba2ad7475820
Mike Swartz <mikelswartz@gmail.com>
2016-11-02 13:45:04 -0400

    Initial commit

---
+++ provision.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+cd /vagrant/
+yum update
+yum install -y vim java
+wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat/jenkins.repo
+rpm --import https://jenkins-ci.org/redhat/jenkins-ci.org.key
+yum install -y jenkins
+service jenkins start
