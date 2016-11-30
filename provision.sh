2e5988737ab3c6ac7e209f04d7bd7e5ce28ed2c4
Mike Swartz <mikelswartz@gmail.com>
2016-11-30 14:07:05 -0500

    Adjusted for java version and env. vars

--- provision.sh
+++ provision.sh
@@ -1,13 +1,25 @@
 #!/bin/bash
 cd /vagrant/
 yum update
-yum install -y vim java git
+yum install -y vim git
+
+yum install -y java-1.8.0-openjdk.x86_64 java-1.8.0-openjdk-devel.x86_64
+
 wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat/jenkins.repo
 rpm --import https://jenkins-ci.org/redhat/jenkins-ci.org.key
 yum install -y jenkins
 service jenkins start
-JAVA_HOME=/usr/java/jdk1.7.0_60
-export JAVA_HOME
-PATH=$JAVA_HOME/bin:$PATH
-export PATH
-
+if [ -f "/vagrant/apache-maven-3.0.5-bin.tar.gz" ]
+then
+	echo apache-maven-3.0.5-bin.tar.gz exists
+else
+	wget http://mirror.cc.columbia.edu/pub/software/apache/maven/maven-3/3.0.5/binaries/apache-maven-3.0.5-bin.tar.gz
+fi
+tar xzf /vagrant/apache-maven-3.0.5-bin.tar.gz -C /usr/local
+cd /usr/local
+sudo ln -s apache-maven-3.0.5 maven
+JAVA_HOME=/usr/lib/jvm/jre-1.8.0-openjdk.x86_64
+M2_HOME=/usr/local/maven
+PATH=${M2_HOME}/bin:$JAVA_HOME/bin:${PATH}
+printf "export JAVA_HOME=${JAVA_HOME}\nexport M2_HOME=/usr/local/maven\nexport PATH=${M2_HOME}/bin:${PATH}" >> /etc/profile.d/maven.sh
+printf "export JAVA_HOME=${JAVA_HOME}\nexport M2_HOME=/usr/local/maven\nexport PATH=${M2_HOME}/bin:${PATH}" > /home/vagrant/.profile
