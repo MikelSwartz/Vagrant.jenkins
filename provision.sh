4cb4edfbeef07ec50a3ed90260d77e7f1e942fb4
Mike Swartz <mikelswartz@gmail.com>
2016-11-30 11:20:53 -0500

    added git to image for PS CBT

--- provision.sh
+++ provision.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd /vagrant/
 yum update
-yum install -y vim java
+yum install -y vim java git
 wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat/jenkins.repo
 rpm --import https://jenkins-ci.org/redhat/jenkins-ci.org.key
 yum install -y jenkins
