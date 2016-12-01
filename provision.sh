966f3eb49b42d9e697b608cdb73b369da6442ed4
Mike Swartz <mikelswartz@gmail.com>
2016-12-01 13:52:05 -0500

    Added code to install spring-boot in the vagrant directory for manual manipulation

--- provision.sh
+++ provision.sh
@@ -3,6 +3,16 @@ cd /vagrant/
 yum update
 yum install -y vim git tree
 
+if [ -f /vagrant/jenkins2 ]
+then
+	echo jenkins2 exists
+else
+	mkdir /vagrant/jenkins2
+	cd /vagrant/jenkins2
+	git clone https://github.com/g0t4/jenkins2-course-spring-boot.git
+	cd
+fi
+
 yum install -y java-1.8.0-openjdk.x86_64 java-1.8.0-openjdk-devel.x86_64
 
 wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat/jenkins.repo
